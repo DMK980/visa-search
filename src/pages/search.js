9aaba86225e2c0b0ca0c7b4039237d84d0b9a0de
DMK980 <derrick.mutuma@gmail.com>
2024-01-08 00:15:30 +0000

    more progress

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,16 +1,16 @@
-
+import "./pages css/search.css"
 export const Search = ()=>{
     return (
         <div className="body-container">
-            <h1>VISA SPONSORSHIP SEARCH</h1>
-            <ul>
+            <h1 className="headline">VISA SPONSORSHIP SEARCH</h1>
+            <ul className="bullet-points">
                 <li>A directory of companies that are licensed to sponsor workers in the UK.</li>
-                <li>Data Collected from the official UK government website</li>
+                <li>Data Collected from the <span className="emphasis-text">official UK government website</span></li>
             </ul>
-            <div className="Search-container">
-                <input id="company-name-input"></input>
-                <button id="search-button"type="submit"></button>
+            <div className="search-container">
+                <input className="company-name-input"placeholder="Company name here"></input>
+                <button className="search-button"type="submit">SEARCH</button>
             </div>
-        </div>
+        </div> 
     )
 }
