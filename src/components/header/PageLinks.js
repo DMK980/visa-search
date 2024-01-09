bdfbefb31c1799625ca2423f9d3a44f05b9c055a
DMK980 <derrick.mutuma@gmail.com>
2024-01-09 12:15:01 +0000

    making website responive

--- src/components/header/PageLinks.js
+++ src/components/header/PageLinks.js
@@ -4,22 +4,12 @@ import "./headercss/PageLinks.css"
 export const Link = (props)=>{
     return (
         <div className="links">
-            <div className="Page-Link-Container one">
-                <a 
-                 className="Page-Link link-one"
-                 href=" ">Search</a>
-                <div 
-                 className="underline" 
-                 id="link-one-underline"></div>
-            </div>
-            <div className="Page-Link-Container two">
-                <a 
-                 className="Page-Link link-two"
-                 href=" ">Stats</a>
-                <div 
-                 className="underline"
-                 id="link-two-underline"></div>
-            </div>
+            <a 
+                className="Page-Link link-one"
+                href=" ">Search</a>
+            <a 
+                className="Page-Link link-two"
+                href=" ">Stats</a>
         </div>
     )
 }
