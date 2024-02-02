4b5697373d1b72536b080a3a5ecec473ef721f2f
DMK980 <derrick.mutuma@gmail.com>
2024-02-02 14:34:09 +0000

    Search component done

--- src/Pages/landing/Landing.js
+++ src/Pages/landing/Landing.js
@@ -1,13 +1,14 @@
 import landing from "./landing.module.css"
 import { Search } from "../../components/Search/search"
 import { HeaderComponent } from "../../components/Header/Header"
-import { Footer } from "../../components/Footer/footer"
+// import { Footer } from "../../components/Footer/footer"
 
 export const LandingPage = ()=>{
     return (
         <div className={landing.landingpage}>
             <HeaderComponent/>
             <section className={landing.section}>
+                <Search/>
             </section>
         </div>
     )
--- src/components/Search/search.js
+++ src/components/Search/search.js
@@ -2,9 +2,9 @@ import search from "./search.module.css"
 
 export const Search = ()=>{
     return(
-        <div className={search.input_container}>
-                <input className={search.input} placeholder="Enter Company name here"type="text"/>
+        <div className={search.search_container}>
+                <input className={search.input} placeholder="Company name here"type="text"/>
                 <button className={search.search}>SEARCH</button>
-        </div>
+        </div> 
     )
 }
