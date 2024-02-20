e510bd7b9852623346253c272e73aacc1374b9bd
DMK980 <derrick.mutuma@gmail.com>
2024-02-20 10:31:07 +0000

    major update pages done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import { LandingPage } from './Pages/landing/Landing';
+import { SearchPage } from './Pages/search/searchpage';
 
 function App() {
 
   return (
     <>
-      <LandingPage/>
+      <SearchPage/>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {createBrowserRouter,RouterProvider} from "react-router-dom"
+import { LandingPage } from './Pages/landing/Landing';
+import { SearchPage } from './Pages/search/searchpage';
+
+const router = createBrowserRouter([
+  {
+    path : "/",
+    element: <LandingPage/>
+  },
+  {
+    path : "/search",
+    element: <SearchPage/>
+  }
+]);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode> 
 );
 
